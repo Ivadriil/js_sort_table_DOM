3cb8f868f20e6cbd65efed39ea53431986309a7e
Ivadriil <petrivi51@gmail.com>
2026-03-25 14:47:56 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,23 +11,35 @@ headTable.addEventListener('click', (e) => {
   }
 
   const rows = Array.from(tableBody.querySelectorAll('tr'));
+  const cellIndex = link.cellIndex;
+  const isNumberColumn = checkIsNumberColumn(rows, cellIndex);
 
-  sortRows(rows, link.cellIndex);
-
-  function sortRows(listenrs, cellIndex) {
-    listenrs.sort((a, b) => {
-      let argA = a.children[cellIndex].textContent.trim();
-      let argB = b.children[cellIndex].textContent.trim();
-
-      if (cellIndex > 1) {
-        argA = Number(argA.replace(/[$,]/g, ''));
-        argB = Number(argB.replace(/[$,]/g, ''));
-
-        return argA - argB;
-      } else {
-        return argA.localeCompare(argB);
-      }
-    });
-    tableBody.append(...listenrs);
-  }
+  sortRows(rows, cellIndex, isNumberColumn);
 });
+
+function sortRows(rows, cellIndex, isNumberColumn) {
+  rows.sort((a, b) => {
+    let argA = a.children[cellIndex].textContent.trim();
+    let argB = b.children[cellIndex].textContent.trim();
+
+    if (isNumberColumn) {
+      argA = Number(argA.replace(/[$,]/g, ''));
+      argB = Number(argB.replace(/[$,]/g, ''));
+
+      return argA - argB;
+    }
+
+    return argA.localeCompare(argB);
+  });
+
+  tableBody.append(...rows);
+}
+
+function checkIsNumberColumn(rows, cellIndex) {
+  return rows.every((row) => {
+    const value = row.children[cellIndex].textContent.trim();
+    const cleanValue = value.replace(/[$,]/g, '');
+
+    return cleanValue !== '' && !Number.isNaN(Number(cleanValue));
+  });
+}
